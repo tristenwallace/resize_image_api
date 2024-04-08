7654d4552535900ca357966ede6931bb43d14b81
tristenwallace <tri3sten@gmail.com>
2024-04-08 10:55:52 -0400

    docs: Better commenting on cacheUtils

--- src/utils/cacheUtils.ts
+++ src/utils/cacheUtils.ts
@@ -1,12 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 
-const cacheDir = path.resolve(__dirname, '../../public/cache'); // Adjust based on your directory structure
+// Define the directory where cached images are stored
+const cacheDir = path.resolve(__dirname, '../../public/cache');
 
+/**
+ * Retrieves the file path of a cached image based on its cache key.
+ *
+ * This function constructs the expected file path for a cached image using the provided cache key.
+ * It then checks if the file exists in the cache directory. If the file exists, the path to the
+ * cached image is returned; otherwise, null is returned to indicate the image is not in the cache.
+ *
+ * @param {string} cacheKey - The unique identifier for the cached image.
+ * @returns {string | null} The file path to the cached image if it exists, otherwise null.
+ */
 export function getCachedImagePath(cacheKey: string): string | null {
+  // Construct the file path for the cached image
   const cachedImagePath = path.join(cacheDir, `${cacheKey}.jpg`);
-  if (fs.existsSync(cachedImagePath)) {
-    return cachedImagePath;
-  }
-  return null;
+
+  // Check for the existence of the cached image and return its path if found
+  return fs.existsSync(cachedImagePath) ? cachedImagePath : null;
 }
